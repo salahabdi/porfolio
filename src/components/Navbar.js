5d1db4dc048f696cc30945711fddf352fe044f00
salahabdi <salahabdirahman0@gmail.com>
2023-03-16 12:43:54 -0400

    N

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import './styles/Navbar.scss';
 import { NavLink } from 'react-router-dom';
 
 
-function Navbar() {
+const Navbar = () =>  {
   return (
     <nav className='navbar'>
       <div className='navbar__button'>
