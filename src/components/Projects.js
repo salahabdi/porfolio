489eabf059bcb393d5f43cfd069c3e6f67ce2500
salahabdi <salahabdirahman0@gmail.com>
2023-03-17 21:09:04 -0400

    updated project page content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,17 +1,28 @@
 import React from "react"
-import Cards from "./Cards"
-import image from './assets/image.png'
+// import Cards from "./Cards"
+// import image from './assets/image.png'
 
 
 export default function Projects() {
     return(
-        <div>
-             <Cards 
-              image={image} 
-              title='Project Title'
-              description='Project Description'
-              />
+        // <div>
+        //      <Cards 
+        //       image={image} 
+        //       title='Project Title'
+        //       description='Project Description'
+        //       />
               
+        // </div>
+
+        <div className="page-content">
+            <div className="content">
+                <p>
+                Project page coming up soon in the mean time please take a look 
+                at my github page.
+                </p>
+
+            </div>
+
         </div>
         
     
